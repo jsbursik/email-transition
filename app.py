14d5b4586852572b3d847b3d7a63ed11034fa40f
Jordan Bursik <jsbursik@gmail.com>
2023-02-21 11:38:40 -0600

    more or less finished

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import os
 import csv
 
 import smtplib
+import ssl
 
-from email import encoders
-from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -14,17 +13,31 @@ load_dotenv()
 sender_email = "noreply@holidayautogroup.com"
 password = os.environ.get("NOREPLY_PASS")
 
-msg = MIMEMultipart("alternative")
+
+text = """
+    You need to view this email in HTML, if you see this, contact Jordan Bursik.
+"""
 
 
-with open('list.csv') as file:
-    reader = csv.DictReader(file, delimiter='\t')
-    for row in reader:
-        msg["Subject"] = f'{row["Display name"]}, sign-in to your new email.'
-        msg["From"] = sender_email
-        msg["To"] = row["Previous Email"]
-        text = """\
-            """
-        html = """\
-            
-            """
+html = open("email.html", "r").read()
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.ipower.com", 465, context=context) as server:
+    print(f"Logging into {sender_email}...")
+    server.login(sender_email, password)
+    with open('list.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            msg = MIMEMultipart("alternative")
+
+            msg["Subject"] = f'{row["Display name"]}, sign-in to your new email.'
+            msg["From"] = sender_email
+            msg["To"] = row["Previous Email"]
+
+            msg.attach(MIMEText(text, "plain"))
+            temp_HTML = html.replace("{user_name}", row["Display name"]).replace(
+                "{user_email}", row["Username"]).replace("{user_pass}", row["Password"]).replace("{login_date}", "March 11th, 2023")
+            msg.attach(MIMEText(temp_HTML, "html"))
+            print(f"Sending email to {row['Display name']}...")
+            server.sendmail(
+                sender_email, row["Previous Email"], msg.as_string())
